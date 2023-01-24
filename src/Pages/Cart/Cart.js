4653e24efcb28a1c16992c81d44bab85a809a7e5
Hemant <hementchaubey@gmail.com>
2023-01-24 09:47:49 +0530

    Added continue shopping functionality and if cart is zero emprty cart msg displayed.

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -3,10 +3,12 @@ import { shopContext } from "../../Context/ShopContext";
 import { Foods } from "../../Food";
 import CartItem from "./CartItem";
 import "./cart.css";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
   const { cartItems, getTotalCartAmount } = useContext(shopContext);
   const totalAmount = getTotalCartAmount();
+  const navigate = useNavigate();
   return (
     <div className="cart">
       <div>
@@ -19,11 +21,15 @@ const Cart = () => {
           }
         })}
       </div>
-      <div className="checkout">
-        <p>Subtotal : ${totalAmount}</p>
-        <button>Continue Shopping</button>
-        <button>Checkout</button>
-      </div>
+      {totalAmount > 0 ? (
+        <div className="checkout">
+          <p>Subtotal : ${totalAmount}</p>
+          <button onClick={() => navigate("/")}>Continue Shopping</button>
+          <button>Checkout</button>
+        </div>
+      ) : (
+        <h1>Your cart Empty</h1>
+      )}
     </div>
   );
 };
